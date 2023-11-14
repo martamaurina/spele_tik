81a9d1f64fe5ab6548381fdf886f5ca38a80b35f
martamaurina <martamaurina3@gmail.com>
2023-11-14 13:26:21 +0200

    Create tiktok.py

---
+++ tiktok.py
@@ -0,0 +1,27 @@
+from tkinter import*
+from tkinter import messagebox #paziņojumi, ieteikumi
+
+mansLogs=Tk()
+mansLogs.title("TicTicToe")
+
+btn1=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
+btn2=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
+btn3=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
+btn4=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
+btn5=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
+btn6=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
+btn7=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
+btn8=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
+btn9=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
+
+
+
+
+
+
+
+
+
+
+
+mansLogs.mainloop()
