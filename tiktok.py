af4c5c1ab2938b0abb03e7b1580671d26e1348be
martamaurina <martamaurina3@gmail.com>
2023-11-16 14:51:53 +0200

    Update tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -5,22 +5,50 @@ from tkinter import messagebox #paziņojumi, ieteikumi
 mansLogs=Tk()
 mansLogs.title("TicTicToe")
 
-def btnClick(button): #padod visu pogu
-global speletajsX, count #kādi mainīgie tiks izmantoti visā programmā
-if button["text"]==""and speletajsX==True: #spēlē X spēlētājs
-    button["text"]="X" #maina uz X
-    speletajsX=False
-    count+=1 #palielina rūtiņu skaitu
+speletajsX=True
+count=0
 
-btn1=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
-btn2=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
-btn3=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
-btn4=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
-btn5=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
-btn6=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
-btn7=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
-btn8=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
-btn9=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24))
+def btnClick(button): #padod visu pogu
+    global speletajsX, count #kādi mainīgie tiks izmantoti visā programmā
+    if button["text"]==""and speletajsX==True: #spēlē X spēlētājs
+        button["text"]="X" #maina uz X
+        speletajsX=False
+        count+=1 #palielina rūtiņu skaitu
+        checkWinner()
+    elif button["text"]=="" and speletajsX==False:
+        button["text"]='O'
+        speletajsX=True
+        count+=1
+        checkWinner()
+    else:
+        messagebox.showerror('TicTocToe', 'Šeit kāds jau ir ieklišķinājis')
+
+def checkWinner():
+    global winner 
+    winner=False #noteikts, ja būs neizškirts
+    if(btn1["text"]=="X" and btn2["text"]=="X" and btn3["text"]=="X" or btn4["text"]=="X" and btn5["text"]=="X" and btn6["text"]=="X" or btn7["text"]=="X" and btn8["text"]=="X" and btn9["text"]=="X" or
+      btn1["text"]=="X" and btn4["text"]=="X" and btn9["text"]=="X" or btn3["text"]=="X"and btn5["text"]=="X" and btn7["text"]=="X" or 
+      btn1["text"]=="X" and btn4["text"]=="X" and btn7["text"]=="X" or btn2["text"]=="X"and btn5["text"]=="X" and btn8["text"]=="X" or btn3["text"]=="X" and btn6["text"]=="X"and btn9["text"]=="X"): #vertikāli
+        winner=True
+        messagebox.showinfo("TicTacToe", "X ir uzvarējtājs")
+    elif(btn1["text"]=="O" and btn2["text"]=="O" and btn3["text"]=="O" or btn4["text"]=="O" and btn5["text"]=="O" and btn6["text"]=="O" or btn7["text"]=="O" and btn8["text"]=="O" and btn9["text"]=="0" or
+      btn1["text"]=="O" and btn4["text"]=="O" and btn9["text"]=="O" or btn3["text"]=="O"and btn5["text"]=="O" and btn7["text"]=="O" or 
+      btn1["text"]=="O" and btn4["text"]=="O" and btn7["text"]=="O" or btn2["text"]=="O"and btn5["text"]=="O" and btn8["text"]=="O" or btn3["text"]=="O" and btn6["text"]=="O"and btn9["text"]=="O"):
+        winner=True
+        messagebox.showinfo("TicTacToe", "O ir uzvarējtājs")
+    elif count==9 and winner==False:
+        messagebox.showinfo("TicTacToe", "Neizšķirts")
+
+
+btn1=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24),command=lambda:btnClick(btn1))
+btn2=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24),command=lambda:btnClick(btn2))
+btn3=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24),command=lambda:btnClick(btn3))
+btn4=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24),command=lambda:btnClick(btn4))
+btn5=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24),command=lambda:btnClick(btn5))
+btn6=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24),command=lambda:btnClick(btn6))
+btn7=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24),command=lambda:btnClick(btn7))
+btn8=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24),command=lambda:btnClick(btn8))
+btn9=Button(mansLogs, text="", width=6, height=3, font=('Helvica', 24),command=lambda:btnClick(btn9))
 
 btn1.grid(row=0, column=0)
 btn2.grid(row=0, column=1)
@@ -31,21 +59,6 @@ btn6.grid(row=1, column=2)
 btn7.grid(row=2, column=0)
 btn8.grid(row=2, column=1)
 btn9.grid(row=2, column=2)
-
-
-
-
-
-
-
-
-
-
-
-#speletjasX=True
-#count=0
-
-
 
 
 
