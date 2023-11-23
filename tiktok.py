b517a666ae9c3c058a63f5dd1e8bc5e8aba7c6a7
martamaurina <martamaurina3@gmail.com>
2023-11-23 14:33:27 +0200

    Update tiktok.py
    3. stunda

--- tiktok.py
+++ tiktok.py
@@ -23,20 +23,56 @@ def btnClick(button): #padod visu pogu
     else:
         messagebox.showerror('TicTocToe', 'Šeit kāds jau ir ieklišķinājis')
 
+def disableButtons():
+    btn1.config(state=DISABLED)
+    btn2.config(state=DISABLED)
+    btn3.config(state=DISABLED)
+    btn4.config(state=DISABLED)
+    btn5.config(state=DISABLED)
+    btn6.config(state=DISABLED)
+    btn7.config(state=DISABLED)
+    btn8.config(state=DISABLED)
+    btn9.config(state=DISABLED)
+    return 0
+
+def reset():
+    btn1.config(state=NORMAL)
+    btn2.config(state=NORMAL)
+    btn3.config(state=NORMAL)
+    btn4.config(state=NORMAL)
+    btn5.config(state=NORMAL)
+    btn6.config(state=NORMAL)
+    btn7.config(state=NORMAL)
+    btn8.config(state=NORMAL)
+    btn9.config(state=NORMAL)
+    btn1['text']=''
+    btn2['text']=''
+    btn3['text']=''
+    btn4['text']=''
+    btn5['text']=''
+    btn6['text']=''
+    btn7['text']=''
+    btn8['text']=''
+    btn9['text']=''
+    return 0
+
 def checkWinner():
     global winner 
     winner=False #noteikts, ja būs neizškirts
     if(btn1["text"]=="X" and btn2["text"]=="X" and btn3["text"]=="X" or btn4["text"]=="X" and btn5["text"]=="X" and btn6["text"]=="X" or btn7["text"]=="X" and btn8["text"]=="X" and btn9["text"]=="X" or
-      btn1["text"]=="X" and btn4["text"]=="X" and btn9["text"]=="X" or btn3["text"]=="X"and btn5["text"]=="X" and btn7["text"]=="X" or 
+      btn1["text"]=="X" and btn5["text"]=="X" and btn9["text"]=="X" or btn3["text"]=="X"and btn5["text"]=="X" and btn7["text"]=="X" or 
       btn1["text"]=="X" and btn4["text"]=="X" and btn7["text"]=="X" or btn2["text"]=="X"and btn5["text"]=="X" and btn8["text"]=="X" or btn3["text"]=="X" and btn6["text"]=="X"and btn9["text"]=="X"): #vertikāli
         winner=True
+        disableButtons()
         messagebox.showinfo("TicTacToe", "X ir uzvarējtājs")
-    elif(btn1["text"]=="O" and btn2["text"]=="O" and btn3["text"]=="O" or btn4["text"]=="O" and btn5["text"]=="O" and btn6["text"]=="O" or btn7["text"]=="O" and btn8["text"]=="O" and btn9["text"]=="0" or
-      btn1["text"]=="O" and btn4["text"]=="O" and btn9["text"]=="O" or btn3["text"]=="O"and btn5["text"]=="O" and btn7["text"]=="O" or 
+    elif(btn1["text"]=="O" and btn2["text"]=="O" and btn3["text"]=="O" or btn4["text"]=="O" and btn5["text"]=="O" and btn6["text"]=="O" or btn7["text"]=="O" and btn8["text"]=="O" and btn9["text"]=="O" or
+      btn1["text"]=="O" and btn5["text"]=="O" and btn9["text"]=="O" or btn3["text"]=="O"and btn5["text"]=="O" and btn7["text"]=="O" or 
       btn1["text"]=="O" and btn4["text"]=="O" and btn7["text"]=="O" or btn2["text"]=="O"and btn5["text"]=="O" and btn8["text"]=="O" or btn3["text"]=="O" and btn6["text"]=="O"and btn9["text"]=="O"):
         winner=True
+        disableButtons()
         messagebox.showinfo("TicTacToe", "O ir uzvarējtājs")
     elif count==9 and winner==False:
+        disableButtons()
         messagebox.showinfo("TicTacToe", "Neizšķirts")
 
 
@@ -59,6 +95,15 @@ btn6.grid(row=1, column=2)
 btn7.grid(row=2, column=0)
 btn8.grid(row=2, column=1)
 btn9.grid(row=2, column=2)
+
+galvenalzvelne=Menu(mansLogs)#izveido galveno izvelni
+mansLogs.config(menu=galvenalzvelne)#pievieno gara...
+
+opcijas=Menu(galvenalzvelne, tearoff=False)#maza izvēlne
+galvenalzvelne.add_cascade(label="Opcijas", menu=opcijas)#lejupkritošais  saraksts
+
+opcijas.add_command(label="Jauna spēle", command=reset)
+opcijas.add_command(label="Iziet", command=mansLogs.quit)
 
 
 
